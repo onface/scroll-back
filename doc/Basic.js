a4a96211ff592e36a184bfc8cedb111314f214ee
Nimo <nimo.jser@gmail.com>
2018-05-07 15:54:00 +0800

    onReleaseStart judgeStart

--- doc/Basic.js
+++ doc/Basic.js
@@ -11,25 +11,49 @@ class Basic extends React.Component {
     componentDidMount() {
         const self = this
         self.scroll = new Scroll({
-            scroll: self.refScroll.offsetHeight,
-            content: self.refContent.offsetHeight,
+            onReleaseStart: function () {
+                if (self.state.offsetY > 30) {
+                    self.scroll.closeStart(30)
+                    setTimeout(function () {
+                        console.log('close')
+                        self.scroll.closeStart()
+                    }, 1000)
+                    return true
+                }
+            },
+            judgeStart: function (mount) {
+                let output = {
+                    isStart: self.state.offsetY + mount > 0,
+                    shouldMount: -self.state.offsetY
+                }
+                output.retard = function () {
+                    return output.shouldMount / self.refsScroll.offsetHeight
+                }
+                return output
+            },
+            judgeEnd: function (mount) {
+                return {
+                    isEnd: self.state.offsetY + mount < (self.refsScroll.offsetHeight - self.refsContent.offsetHeight),
+                    shouldMount: (self.refsScroll.offsetHeight - self.refsContent.offsetHeight) - self.state.offsetY
+                }
+            },
             onChange: function (mount) {
                 self.setState({
                     offsetY: self.state.offsetY + mount
                 })
             }
         })
-        self.refScroll.addEventListener('touchstart', function (e) {
+        self.refsScroll.addEventListener('touchstart', function (e) {
             e.preventDefault()
             e.stopPropagation()
             self.scroll.touchStart(e.touches[0].clientY)
         })
-        self.refScroll.addEventListener('touchmove', function (e) {
+        self.refsScroll.addEventListener('touchmove', function (e) {
             e.preventDefault()
             e.stopPropagation()
             self.scroll.touchMove(e.touches[0].clientY)
         })
-        self.refScroll.addEventListener('touchend', function (e) {
+        self.refsScroll.addEventListener('touchend', function (e) {
             e.preventDefault()
             e.stopPropagation()
             self.scroll.touchEnd()
@@ -39,17 +63,20 @@ class Basic extends React.Component {
         const self = this
         return (
             <div className="basicDemo" >
-                <div className="scroll" ref={(node) => {self.refScroll = node}}>
+                <div className="scroll" ref={(node) => {self.refsScroll = node}}
+                    style={{border: '1px solid blue', height:'10em'}}
+                >
                     <div
                         className="content"
-                        ref={(node) => {self.refContent = node}}
+                        ref={(node) => {self.refsContent = node}}
                         style={{
-                            transform: `translateX(0px) translateY(${self.state.offsetY}px) translateZ(0px)`
+                            transform: `translateX(0px) translateY(${self.state.offsetY}px) translateZ(0px)`,
+                            border: '1px solid orange'
                         }}
                      >
                          {self.state.offsetY}
                         ...
-                        <div style={{height: '10em', border: '1px solid red'}} ></div>
+                        <div style={{height: '24em', borderLeft: '1px solid red'}} ></div>
                         ...
                     </div>
                 </div>
